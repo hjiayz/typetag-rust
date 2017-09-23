1038a3758a6db63ec3a4a65e4907bca4bdc742f8
hjiayz <hjiayz@hotmail.com>
2017-09-23 18:59:22 +0800

    fix

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ module.exports = (t) => {
     t.generic.verify((o, p) => Array.isArray(o) && (o.length == p[1]) && (o.every((v) => p[0].is(v)))).paramtype(i.tuple_type_usize).define("array");
     i.array.new([i.type, 2], "array_type_2");
     t.generic.verify((o, p) => o.constructor === Map && Array.from(o).every(v => p[0].is(v[0]) && p[1].is(v[1]))).paramtype(i.array_type_2).define("hashmap");
-    i.hashmap.new([i.string, i.type], "map_string_type");
-    t.generic.verify((o, p) => (typeof o == "object") && (Object.keys(o).length === p.size) && Array.from(p).every(p => p[1].is(o[p[0]]))).paramtype(i.map_string_type).define("struct");
-    i.hashmap.new([i.string, i.slice_type], "map_string_slice_type");
+    t.generic.verify((o, p) => o.constructor === Object && Object.values(o).every(v => p.is(v))).paramtype(i.type).define("hashmap_string");
+    i.hashmap_string.new(i.type, "map_string_type");
+    t.generic.verify((o, p) => (typeof o == "object") && (Object.keys(o).length === Object.keys(p).length) && Object.keys(p).every(k => p[k].is(o[k]))).paramtype(i.map_string_type).define("struct");
+    i.hashmap_string.new(i.slice_type, "map_string_slice_type");
     t.generic.verify((o, p) => {
-        if (typeof o == "string") return (p.has(o)) && (p.get(o).length == 0);
+        if (typeof o == "string") return p[o].length == 0;
         if (typeof o != "object") return false;
         let lit = Object.keys(o)[0];
-        let types = p.get(lit);
+        let types = p[lit];
         let val = o[lit];
         if (types.length == 1) return types[0].is(val);
         return Array.isArray(val) && (types.length == val.length) && types.every((v, i) => v.is(val[i]))
--- test/test.js
+++ test/test.js
@@ -11,11 +11,11 @@ i.array.new([i.u8, 3], "array_u8_3");
 i.array_u8_3.assert([1, 2, 22])
 i.hashmap.new([i.i8, i.i8], "hashmap_i8_i8")
 i.hashmap_i8_i8.assert(new Map([[1, 92], [3, 4]]));
-i.struct.new(new Map([["name", i.string], ["age", i.i8]]), "struct_name_age");
+i.struct.new({ "name": i.string, "age": i.i8 }, "struct_name_age");
 i.struct_name_age.assert({ name: "hjiayz", age: 90 });
-i.enum.new(new Map([["none", []], ["some", [i.string]]]), "enum_option_string");
+i.enum.new({ "none": [], "some": [i.string] }, "enum_option_string");
 i.enum_option_string.assert("none");
 i.enum_option_string.assert({ "some": "nice" });
-i.enum.new(new Map([["none", []], ["some", [i.string, i.string]]]), "enum_option_string_string");
+i.enum.new({ "none": [], "some": [i.string, i.string] }, "enum_option_string_string");
 i.enum_option_string_string.assert("none");
 i.enum_option_string_string.assert({ "some": ["nice", "boat"] });
