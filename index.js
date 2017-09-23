37f1c1592514b74c465ca387a3965e19a3d11354
hjiayz <hjiayz@hotmail.com>
2017-09-23 11:42:34 +0800

    add

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ module.exports = (t) => {
     t.raw
         .verify((o) => isInt(o) && o < 256 && o >= 0).define("u8")
         .verify((o) => isInt(o) && o < (1 << 16) && o >= 0).define("u16")
-        .verify((o) => isInt(o) && o < (1 << 32) && o >= 0).define("u32").define("usize")
+        .verify((o) => isInt(o) && o <= (-1 >>> 0) && o >= 0).define("u32").define("usize")
         .verify((o) => isInt(o) && o < (1 << 7) && o >= (-1 << 7)).define("i8")
         .verify((o) => isInt(o) && o < (1 << 15) && o >= (-1 << 15)).define("i16")
-        .verify((o) => isInt(o) && o < (1 << 31) && o >= (-1 << 31)).define("i32").define("isize")
+        .verify((o) => isInt(o) && o <= (-1 >>> 1) && o >= (-1 << 31)).define("i32").define("isize")
         .verify((o) => typeof o == "number").define("f64")
+        .verify((o) => typeof o == "string").define("string")
     t.generic.verify((o, p) => Array.isArray(o) && o.every((v) => p.is(v))).paramtype(i.type).define("slice");
     i.slice.new(i.type, "slice_type");
     t.generic.verify((o, p) => Array.isArray(o) && (o.length == p.length) && o.every((v, i) => p[i].is(v))).paramtype(i.slice_type).define("tuple");
-
+    i.tuple.new([i.type, i.usize], "tuple_type_usize");
+    t.generic.verify((o, p) => Array.isArray(o) && (o.length == p[1]) && (o.every((v) => p[0].is(v)))).paramtype(i.tuple_type_usize).define("array");
+    i.array.new([i.type, 2], "array_type_2");
+    t.generic.verify((o, p) => o.constructor === Map && Array.from(o).every(v => p[0].is(v[0]) && p[1].is(v[1]))).paramtype(i.array_type_2).define("hashmap");
+    i.hashmap.new([i.string, i.type], "map_string_type");
+    t.generic.verify((o, p) => (typeof o == "object") && (Object.keys(o).length === p.size) && Array.from(p).every(p => p[1].is(o[p[0]]))).paramtype(i.map_string_type).define("struct");
+    i.hashmap.new([i.string, i.slice_type], "map_string_slice_type");
+    t.generic.verify((o, p) => {
+        if (typeof o == "string") return (p.has(o)) && (p.get(o).length == 0);
+        if (typeof o != "object") return false;
+        let lit = Object.keys(o)[0];
+        let types = p.get(lit);
+        let val = o[lit];
+        if (types.length == 1) return types[0].is(val);
+        return Array.isArray(val) && (types.length == val.length) && types.every((v, i) => v.is(val[i]))
+    }).paramtype(i.map_string_slice_type).define("enum");
 }
--- test/test.js
+++ test/test.js
@@ -1,7 +1,21 @@
 let assert = require("assert");
-let t = new (require("typetag"))();
+let t = new (require("typetag"))(true);
 let i = t.index;
 let rust = require("../index.js");
 t.load(rust);
 assert(!i.i16.is(1 << 15));
 assert(i.i16.is(1 << 15 - 1));
+i.tuple.new([i.u8, i.u16, i.i32], "tuple_u8_u16_i32");
+i.tuple_u8_u16_i32.assert([255, 111, -100]);
+i.array.new([i.u8, 3], "array_u8_3");
+i.array_u8_3.assert([1, 2, 22])
+i.hashmap.new([i.i8, i.i8], "hashmap_i8_i8")
+i.hashmap_i8_i8.assert(new Map([[1, 92], [3, 4]]));
+i.struct.new(new Map([["name", i.string], ["age", i.i8]]), "struct_name_age");
+i.struct_name_age.assert({ name: "hjiayz", age: 90 });
+i.enum.new(new Map([["none", []], ["some", [i.string]]]), "enum_option_string");
+i.enum_option_string.assert("none");
+i.enum_option_string.assert({ "some": "nice" });
+i.enum.new(new Map([["none", []], ["some", [i.string, i.string]]]), "enum_option_string_string");
+i.enum_option_string_string.assert("none");
+i.enum_option_string_string.assert({ "some": ["nice", "boat"] });
